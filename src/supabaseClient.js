24d01426af073ec3266a5234ab889b4e5741ff03
dlworkforcesolutions <dlworkforcesolutions@gmail.com>
2026-04-26 00:47:24 -0500

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://medkwxkiuqqgmodolka.supabase.co";
 
-const supabaseAnonKey = "sb_publishable_UYiGpPFxnNbi7vste1qR0w_7JRxvEgI";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1lZGt3eGt4aXVxcWdtb2RvbGthIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxNzMxNzgsImV4cCI6MjA5Mjc0OTE3OH0.aYu6URJ9I_MuU8I-P7NhGRrHBKzPeWb1_viToS1-Gds";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
